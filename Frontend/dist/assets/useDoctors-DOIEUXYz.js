e152c0bbbb6a118e144ff4cebb9aa68411d1f8a4
feminafiroz <feminafiroz123@gmail.com>
2024-07-11 17:52:12 +0530

    advertisement link done

---
+++ Frontend/dist/assets/useDoctors-DOIEUXYz.js
@@ -0,0 +1,1 @@
+import{r as t,B as c}from"./index-BZQwGW2S.js";import{a as e}from"./axiosService-D_CQMRTJ.js";const u=()=>{const[r,s]=t.useState([]);return t.useEffect(()=>{e.get(c+"/doctors").then(({data:o})=>{console.log(o),s(o.doctors)}).catch(o=>console.log(o))},[s]),{doctors:r,setDoctors:s}};export{u};
