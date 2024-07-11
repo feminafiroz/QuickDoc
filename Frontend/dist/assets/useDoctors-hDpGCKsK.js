8dbf4ff033c1ecf5ed00a30ec5b706259d44a72d
feminafiroz <feminafiroz123@gmail.com>
2024-07-11 16:11:33 +0530

    url changed

--- Frontend/dist/assets/useDoctors-hDpGCKsK.js
+++
@@ -1,1 +0,0 @@
-import{r as t,B as c}from"./index-Cr8a0Wej.js";import{a as e}from"./axiosService-DLfARfzk.js";const u=()=>{const[r,s]=t.useState([]);return t.useEffect(()=>{e.get(c+"/doctors").then(({data:o})=>{console.log(o),s(o.doctors)}).catch(o=>console.log(o))},[s]),{doctors:r,setDoctors:s}};export{u};
